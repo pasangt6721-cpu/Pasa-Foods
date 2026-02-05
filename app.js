742c550f1392e3720ae750f260562ef45f2d12b4
Pasa673__ <pasangt6721@gmail.com>
2026-02-05 20:41:00 +0545

    setups and installing

---
+++ app.js
@@ -0,0 +1,8 @@
+
+// Import assets
+import { assets, menu_list, food_list } from './assets/frontend_assets/assets.js';
+
+console.log("App initialized");
+console.log("Assets loaded:", assets);
+
+// Base application logic can go here
